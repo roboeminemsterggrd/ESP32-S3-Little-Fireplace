950b5f7556e4cd8843e44652f004b49d1ddf0758
Cyrus Yiu <UnsignedArduino@outlook.com>
2024-12-01 22:19:42 -0500

    TFT_eSPI nice

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,29 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_ST7735.h>
 #include <Arduino.h>
 #include <SPI.h>
+#include <TFT_eSPI.h>
 
 const uint8_t LED = 48;
 
-const uint8_t TFT_SCLK = 13;
-const uint8_t TFT_MOSI = 12;
-const uint8_t TFT_RST = 11;
-const uint8_t TFT_DC = 10;
-const uint8_t TFT_CS = 9;
+//const uint8_t TFT_SCLK = 13;
+//const uint8_t TFT_MOSI = 12;
+//const uint8_t TFT_RST = 11;
+//const uint8_t TFT_DC = 10;
+//const uint8_t TFT_CS = 9;
 
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+TFT_eSPI tft = TFT_eSPI();
 
-void setup() {
+[[noreturn]] void setup() {
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
   
-  tft.initR(INITR_18BLACKTAB);
+  tft.init();
   tft.setRotation(1);
-  tft.setSPISpeed(40000000);
+  tft.fillScreen(TFT_BLACK);
+  
+  tft.setCursor(0, 0);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.print("Hello, world!");
   
   while (true) {
     ;
